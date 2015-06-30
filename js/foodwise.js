5b6cf9bb431516774db6c3a3c5da8e7d042bd1e6
David Alicea <david.aliicea@incatech-corp.com>
2015-06-30 13:26:22 -0400

    Activated Subscribe button to redirect to FoodSafey.gov Recall Alert signup.

---
+++ js/foodwise.js
@@ -0,0 +1,15 @@
+/*! jQuery v1.11.1 | (c) 2005, 2014 jQuery Foundation, Inc. 
+ * | jquery.org/license */
+;(function($, window, document, undefined) {
+    $(document).ready(function() {
+        var emailSubscribeFormWrap = $("#foodRecallSubscription_form");
+        var subscriberEmail = $("#email");
+        
+        $("#btnSubscribe", emailSubscribeFormWrap).click(function() {
+            //alert(subscriberEmail.val());
+            window.location.href = 'https://public.govdelivery.com/accounts/USHHSFS/subscribers/qualify?topic_id=USHHSFS_5&email='+subscriberEmail.val() +'&commit=Submit';
+            //return false;
+        });
+    });
+    
+}(jQuery, window, document));
