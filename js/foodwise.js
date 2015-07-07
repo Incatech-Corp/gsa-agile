b244a9777c17a377499ec169aae124da30543d34
Harry Jones <harry.jones@incatech-corp.com>
2015-07-07 12:48:21 -0400

    Scroll to top button

--- js/foodwise.js
+++ js/foodwise.js
@@ -9,7 +9,24 @@
             //alert(subscriberEmail.val());
             window.location.href = 'https://public.govdelivery.com/accounts/USHHSFS/subscribers/qualify?topic_id=USHHSFS_5&email='+subscriberEmail.val() +'&commit=Submit';
             //return false;
+            
         });
+        
+        var offset = 250;
+        var duration = 300;
+        jQuery(window).scroll(function() {
+            if (jQuery(this).scrollTop() > offset) {
+                jQuery('#scroll-up').fadeIn(duration);
+            } else {
+                jQuery('#scroll-up').fadeOut(duration);
+            }
+        });
+
+        jQuery('#scroll-up').click(function(event) {
+            event.preventDefault();
+            jQuery('html, body').animate({scrollTop: 0}, duration);
+            return false;
+        })
     });
     
 }(jQuery, window, document));
